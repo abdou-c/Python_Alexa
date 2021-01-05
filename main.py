d862fedb336a7a7cfed30eafa5e543db4299a11e
Abdou sow <soowzididi84@gmail.com>
2021-01-05 19:00:04 +0000

    alexa python

---
+++ main.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty('voices', voices.id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Entrain d'ecouter...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "alexa" in command:
+                command = command.replace("alexa", "")
+                talk(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if "play" in command:
+        song = command.replace('play', '')
+        talk("playing" + song)
+        pywhatkit.playonyt(song)
+    elif "time" in command:
+        time = datetime.datetime.now().strftime("%Hheure:%Mminute:%Sseconde")
+        talk("Il est" + time)
+
+    elif 'who is' in command:
+        person = command.replace("qui est", "")
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif "date" in command:
+        talk("sorry i have a headache")
+    elif "are you single?" in command:
+        talk("Yes i'm with you")
+    elif "joke" in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk("Can you say it again please!")
+
+while True:
+    run_alexa()
